1347e0740807fc131bf546a062632a5283aa265f
randomrationalizer <lifelower@gmail.com>
2019-02-03 20:40:27 +0300

    Добавляет правки по замечаниям

--- index.php
+++ index.php
@@ -15,51 +15,43 @@ $user_name = 'Маргарита';
 <?php
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
 $lots = [
-    0 => [
+    [
         "name" => "2014 Rossignol District Snowboard",
         "category" => "Доски и лыжи",
         "price" => 10999,
         "img" => "img/lot-1.jpg"
     ],
-    1 => [
+    [
         "name" => "DC Ply Mens 2016/2017 Snowboard",
         "category" => "Доски и лыжи",
         "price" => 159999,
         "img" => "img/lot-2.jpg"
     ],
-    2 => [
+    [
         "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
         "category" => "Крепления",
         "price" => 8000,
         "img" => "img/lot-3.jpg"
     ],
-    3 => [
+    [
         "name" => "Ботинки для сноуборда DC Mutiny Charocal",
         "category" => "Ботинки",
         "price" => 10999,
         "img" => "img/lot-4.jpg"
     ],
-    4 => [
+    [
         "name" => "Куртка для сноуборда DC Mutiny Charocal",
         "category" => "Одежда",
         "price" => 7500,
         "img" => "img/lot-5.jpg"
     ],
-    5 => [
+    [
         "name" => "Маска Oakley Canopy",
         "category" => "Разное",
         "price" => 5400,
         "img" => "img/lot-6.jpg"
     ]
 ];
-
-function show_lots ($items) {
-    $html = "";
-    foreach ($items as $key => $val) {
-        $html .= "<li class='lots__item lot'><div class='lot__image'><img src='" . $val["img"] . "' width='350' height='260' alt='" . $val["name"] . "'></div><div class='lot__info'><span class='lot__category'>" . $val['category'] . "</span><h3 class='lot__title'><a class='text-link' href='pages/lot.html'>" . $val["name"] . "</a></h3><div class='lot__state'><div class='lot__rate'><span class='lot__amount'>Стартовая цена</span><span class='lot__cost'>" . $val["price"] . "<b class='rub'>р</b></span></div><div class='lot__timer timer'>12:23</div></div></div></li>";
-    }
-    return $html;
-};          
 ?>
 <div class="page-wrapper">
 
@@ -99,9 +91,9 @@ function show_lots ($items) {
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-        <?php foreach ($categories as $key => $val) : ?>
+        <?php foreach ($categories as $category) : ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$val ;?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$category ;?></a>
             </li>
         <?php endforeach; ?>
         </ul>
@@ -111,7 +103,26 @@ function show_lots ($items) {
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?=show_lots($lots); ?>
+        <?php foreach ($lots as $lot) : ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=$lot["img"]; ?>" width="350" height="260" alt="<?=$lot["name"]; ?>">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=$lot["category"]; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot["name"]; ?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?=$lot["price"]; ?><b class="rub">р</b></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            12:23
+                        </div>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
         </ul>
     </section>
 </main>
@@ -120,9 +131,9 @@ function show_lots ($items) {
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-        <?php foreach ($categories as $key => $val) : ?>
+        <?php foreach ($categories as $category) : ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$val ;?></a>
+                <a href="pages/all-lots.html"><?=$category ;?></a>
             </li>
         <?php endforeach; ?>
         </ul>
