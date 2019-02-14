b7bced3043679423654d51430cfd908ab2fb5ce6
randomrationalizer <lifelower@gmail.com>
2019-02-15 00:00:53 +0300

    Исправляет схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -1,40 +1,41 @@
-CREATE DATABASE yeticave407511
+CREATE DATABASE `407511-yeticave`
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-USE yeticave407511;
+USE `yeticave407511`;
 
-CREATE TABLE category (
-    category_name CHAR(64) PRIMARY KEY NOT NULL
+CREATE TABLE `category` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE lot (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    add_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    lot_name CHAR(255) NOT NULL,
-    lot_description TEXT(500) NOT NULL,
-    img_path CHAR(255) NOT NULL,
-    start_price INT(9) NOT NULL,
-    ending_date TIMESTAMP NOT NULL,
-    step INT(5) NOT NULL,
-    autor_id INT(10) NOT NULL,
-    winner_id INT(10),
-    category CHAR(64) NOT NULL
+CREATE TABLE `lot` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `add_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT(500) NOT NULL,
+    `img_path` VARCHAR(255) NOT NULL,
+    `start_price` INT NOT NULL,
+    `end_date` TIMESTAMP NOT NULL,
+    `step` INT NOT NULL,
+    `author_id` INT NOT NULL,
+    `winner_id` INT DEFAULT NULL,
+    `category_id` INT NOT NULL
 );
 
-CREATE TABLE bid (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    bid_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    bid_price INT(9) NOT NULL,
-    user_id INT(10) NOT NULL,
-    lot_id INT(20) NOT NULL
+CREATE TABLE `bid` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `price` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `lot_id` INT NOT NULL
 );
 
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    email CHAR(128) NOT NULL UNIQUE,
-    username CHAR(128) NOT NULL,
-    password CHAR(64) NOT NULL,
-    avatar_path CHAR(255),
-    contacts CHAR(255) NOT NULL
+CREATE TABLE `user` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `email` VARCHAR(128) NOT NULL UNIQUE,
+    `username` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `avatar_path` VARCHAR(255),
+    `contacts` VARCHAR(255) NOT NULL
 );
