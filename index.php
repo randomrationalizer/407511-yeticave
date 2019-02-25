2a71efb6c36c59b2e5d67dcc16467303926c9b09
randomrationalizer <lifelower@gmail.com>
2019-02-25 23:25:58 +0300

    Добавляет правки по замечаниям

--- index.php
+++ index.php
@@ -3,12 +3,8 @@ require_once("connection.php");
 require_once("functions.php");
 require_once("data.php");
 
-if ($link) {
-    mysqli_set_charset($link, "utf8");
-
-    $categories = get_data($link, $categories_sql);
-    $lots = get_data($link, $lots_sql);
-}
+$categories = get_data($link, $categories_sql);
+$lots = get_data($link, $lots_sql);
 
 $page_content = include_template("index.php", ["lots" => $lots, "categories" => $categories]);
 $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Главная", "user_name" => $user_name, "categories" => $categories]);
--- lot.php
+++ lot.php
@@ -4,22 +4,18 @@ require_once("connection.php");
 require_once("functions.php");
 require_once("data.php");
 
-if ($link) {
-    mysqli_set_charset($link, "utf8");
+if (isset($_GET['id'])) {
 
-    if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $lot = get_lot_by_id($link, $id);
+    if (!is_null($lot['id'])) {
+        $page_content = include_template("lot.php", ["lot" => $lot]);
+        $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => $lot["name"], "user_name" => $user_name, "categories" => $categories]);
 
-        $id = intval($_GET['id']);
-        $lot = get_lot_by_id($link, $id);
-        if (!is_null($lot['id'])) {
-            $page_content = include_template("lot.php", ["lot" => $lot]);
-            $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => $lot["name"], "user_name" => $user_name, "categories" => $categories]);
-
-        } else {
-            http_response_code(404);
-            $page_content = include_template("error.php", ["error_header" => "404 Страница не найдена", "error_text" => "Данной страницы не существует на сайте."]);
-            $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "404 Страница не найдена", "user_name" => $user_name, "categories" => $categories]);
-        }
+    } else {
+        http_response_code(404);
+        $page_content = include_template("error.php", ["error_header" => "404 Страница не найдена", "error_text" => "Данной страницы не существует на сайте."]);
+        $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "404 Страница не найдена", "user_name" => $user_name, "categories" => $categories]);
     }
 }
 
