6d0d5d3ce0e045a20f8c364b0575d41f4064ee59
randomrationalizer <lifelower@gmail.com>
2019-03-05 00:21:53 +0300

    Добавляет правки по замечаниям Исправлена проверка корректности даты окончания аукциона Добавлена проверка категории на существование Убраны лишние блоки условий

--- add.php
+++ add.php
@@ -27,6 +27,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errors["category"] = "Выберите категорию";
     }
 
+    // Проверка на существование категории в БД
+    $sql_check_category = "SELECT * FROM `category` WHERE `category`.`id` =" . $new_lot["category"];
+    if (!count(get_data($link, $sql_check_category))) {
+        $errors["category"] = "Ошибка БД: выбранной категории не существует";
+    };
+
     // Проверка значений цены и ставки
     foreach ($integer_fields as $field) {
         if (isset($new_lot[$field]) && (!filter_var($new_lot[$field], FILTER_VALIDATE_INT) || $new_lot[$field] < 0)) {
@@ -35,8 +41,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Проверка корректности даты окончания аукциона
-    if (strtotime($new_lot["end_date"]) < strtotime('tomorrow')) {
-        $errors["end_date"] = "Введена некорректная дата завершения аукциона";
+    $min_lot_lifetime = 86400;
+    $start_date = time();
+    $end_date = strtotime($new_lot["end_date"]);
+
+    if ($end_date - $start_date < $min_lot_lifetime) {
+        $errors["end_date"] = "Дата окончания торгов должна быть больше текущей хотя бы на 1 сутки";
     }
 
     // Проверка загруженных изображений
@@ -66,28 +76,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $page_content = include_template("add.php", ["lot" => $new_lot, "errors" => $errors, "categories" => $categories]);
         $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Добавить лот", "user_name" => $user_name, "categories" => $categories]);
         print($layout_content);
-    } else {
-        // Если форма заполенена правильно
-        $sql_add_lot = "INSERT INTO `lot` (`start_date`, `name`, `description`, `img_path`, `start_price`, `end_date`, `step`, `author_id`, `winner_id`, `category_id`) VALUES (NOW(), ?, ?, ?, ?, ?, ?, 1, NULL, ?)";
-
-        $stmt = db_get_prepare_stmt($link, $sql_add_lot, [$new_lot['name'], $new_lot['description'], $new_lot["img_path"], $new_lot["start_price"], $new_lot["end_date"], $new_lot['step'], $new_lot['category']]);
-
-        $res = mysqli_stmt_execute($stmt);
-
-        if ($res) {
-            $lot_id = mysqli_insert_id($link);
-            header("Location: lot.php?id=" . $lot_id);
-        } else {
-            $page_content = include_template("error.php", ["error_header" => "Ошибка запроса", "error_text" => mysqli_error($link)]);
-            $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Ошибка отправки формы", "user_name" => $user_name, "categories" => $categories]);
-            print($layout_content);
-        }
+        exit;
     }
-} else {
-    // Если форма не отправлена, выводит страницу с пустой формой
-    $page_content = include_template("add.php", ["lot" => $new_lot, "errors" => $errors, "categories" => $categories]);
-    $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Добавить лот", "user_name" => $user_name, "categories" => $categories]);
+    
+    // Если форма заполенена правильно
+    $sql_add_lot = "INSERT INTO `lot` (`start_date`, `name`, `description`, `img_path`, `start_price`, `end_date`, `step`, `author_id`, `winner_id`, `category_id`) VALUES (NOW(), ?, ?, ?, ?, ?, ?, 1, NULL, ?)";
+
+    $stmt = db_get_prepare_stmt($link, $sql_add_lot, [$new_lot['name'], $new_lot['description'], $new_lot["img_path"], $new_lot["start_price"], $new_lot["end_date"], $new_lot['step'], $new_lot['category']]);
+
+    $res = mysqli_stmt_execute($stmt);
+
+    if ($res) {
+        $lot_id = mysqli_insert_id($link);
+        header("Location: lot.php?id=" . $lot_id);
+    }
+        
+    $page_content = include_template("error.php", ["error_header" => "Ошибка запроса", "error_text" => mysqli_error($link)]);
+    $layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Ошибка отправки формы", "user_name" => $user_name, "categories" => $categories]);
     print($layout_content);
+
+    exit;
 }
+
+// Если форма не отправлена, выводит страницу с пустой формой
+$page_content = include_template("add.php", ["lot" => $new_lot, "errors" => $errors, "categories" => $categories]);
+$layout_content = include_template("layout.php", ["page_content" => $page_content, "page_title" => "Добавить лот", "user_name" => $user_name, "categories" => $categories]);
+print($layout_content);
 
 ?>
--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+require_once("mysql_helper.php");
+
 function include_template($name, $data) {
     $name = 'templates/' . $name;
     $result = '';
@@ -75,50 +77,6 @@ function show_time_left ($start, $end) {
     $end_time = new DateTime($end);
     $time_left = date_interval_format(date_diff($start_time, $end_time), "%H:%I");
     return $time_left;
-}
-
-/**
- * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
- *
- * @param $link mysqli Ресурс соединения
- * @param $sql string SQL запрос с плейсхолдерами вместо значений
- * @param array $data Данные для вставки на место плейсхолдеров
- *
- * @return mysqli_stmt Подготовленное выражение
- */
-function db_get_prepare_stmt($link, $sql, $data = []) {
-    $stmt = mysqli_prepare($link, $sql);
-
-    if ($data) {
-        $types = '';
-        $stmt_data = [];
-
-        foreach ($data as $value) {
-            $type = null;
-
-            if (is_int($value)) {
-                $type = 'i';
-            }
-            else if (is_string($value)) {
-                $type = 's';
-            }
-            else if (is_double($value)) {
-                $type = 'd';
-            }
-
-            if ($type) {
-                $types .= $type;
-                $stmt_data[] = $value;
-            }
-        }
-
-        $values = array_merge([$stmt, $types], $stmt_data);
-
-        $func = 'mysqli_stmt_bind_param';
-        $func(...$values);
-    }
-
-    return $stmt;
 }
 
 ?>
